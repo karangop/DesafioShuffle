6e9d05fc52081409d3d39ab20a281d5286595198
karangop <karangop@gmail.com>
2017-04-17 19:59:10 -0300

    ADD:  permissions granted

--- app/src/main/java/com/karangop/desafioshuffle/MainActivity.java
+++ app/src/main/java/com/karangop/desafioshuffle/MainActivity.java
@@ -1,12 +1,16 @@
 package com.karangop.desafioshuffle;
 
+import android.Manifest;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
-import android.os.IBinder;
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +20,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private PlayerService playerService;
     private ServiceConnection serviceConnection;
     private boolean isBound = false;
+
+    private static final int RC_EXTERNAL = 343;
+
 
     private void bindToPlayer(){
         Intent intent = new Intent(this, PlayerService.class);
@@ -46,7 +53,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bindToPlayer();
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+            requestPermissions(permissions, RC_EXTERNAL);
+        }else{
+            bindToPlayer();
+        }
+
+
 
         Button button = (Button) findViewById(R.id.playBtn);
         button.setOnClickListener(this);
@@ -75,6 +90,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         if (isBound){
             playerService.playSongs();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        Log.d("REUEST CODE", String.valueOf(requestCode));
+        Log.d("PERMISSIONS", permissions[0]);
+        Log.d("RESULT", String.valueOf(grantResults[0]));
+        if (RC_EXTERNAL == requestCode){
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                bindToPlayer();
+            }
         }
     }
 }
